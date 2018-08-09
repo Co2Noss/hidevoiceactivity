72d45a7055477145f02ab7db9f654f8b0a3da0b4
Unknown <Co2Noss@gmail.com>
2018-08-09 00:01:45 -0400

    v1.0
    Hides the voice activity UI element.

---
+++ code.lua
@@ -0,0 +1,4 @@
+-- This file is loaded from "Hide Blizzard Voice Activity.toc"
+
+-- This is a comment, you can replace it with the lua code that you wish to make into an Addon!
+VoiceActivityManager:Hide()
